8c3e6e75e3701c83951d0d43f8a45e46d926de4f
Chris J Mears <chris.mears@gmail.com>
2023-03-04 22:23:17 -0800

    Test > Debugging

--- tests/router.spec.ts
+++ tests/router.spec.ts
@@ -2,6 +2,10 @@ import { test, expect } from "@playwright/test";
 
 test("should return breweries", async ({ request }) => {
   const response = await request.get(`/v2/breweries`);
+  console.log(
+    "Response Status: ",
+    `${response.status()} - ${response.statusText()}`
+  );
   expect(response.ok()).toBeTruthy();
 
   const breweries = (await response.json()).result.data;
